9cf215840264c2f0c5cd0ab2507dec74c3e8deb5
bharath856 <99651027+bharath856@users.noreply.github.com>
2022-06-29 11:09:25 +0530

    def rev(a):     re=0     while a>0:         r=a%10         re=(re*10)+r         a//=10     return re a=int(input()) c=0 if a<0:     c=1     a*=-1 res=rev(a) if c==1:     print(res*-1) else:     print(res)

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,10 +1,10 @@
 def rev(a):
-  re=0
-  while a>0:
-     r=a%10
-     re=(re*10)+r
-     a//=10
-  return re
+    re=0
+    while a>0:
+        r=a%10
+        re=(re*10)+r
+        a//=10
+    return re
 a=int(input())
 c=0
 if a<0:
@@ -12,6 +12,6 @@ if a<0:
     a*=-1
 res=rev(a)
 if c==1:
-   print(res*-1)
+    print(res*-1)
 else:
-   print(res)
+    print(res)
