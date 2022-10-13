1674db76475c05f7a87043cba1d7a1bdc00717ea
bharath856 <99651027+bharath856@users.noreply.github.com>
2022-10-13 20:33:32 +0530

    s=input() s=list(s) if ('a' in s and 'e' in s and 'i' in s and 'o' in s and 'u' in s)or('A' in s and 'E' in s and 'I' in s and 'O' in s and 'U' in s):     print(True) else:     print(False)

---
+++ All_vowels_in_a_string.py
@@ -0,0 +1,6 @@
+s=input()
+s=list(s)
+if ('a' in s and 'e' in s and 'i' in s and 'o' in s and 'u' in s)or('A' in s and 'E' in s and 'I' in s and 'O' in s and 'U' in s):
+    print(True)
+else:
+    print(False)
