6697adc4b5eafb00c78f753d21627d9ba43e7086
bharath856 <99651027+bharath856@users.noreply.github.com>
2022-10-13 20:45:02 +0530

    s=input() s=s.lower() s=s.split(' ') a=[] for j in s:     c=0     x=list(j)     for i in x:         if i=='a'or i=='e' or i=='i' or i=='o' or i=='u':             c+=1     a.append(c) print(min(a))

---
+++ minimum_vowel_count_in_a_word.py
@@ -0,0 +1,12 @@
+s=input()
+s=s.lower()
+s=s.split(' ')
+a=[]
+for j in s:
+    c=0
+    x=list(j)
+    for i in x:
+        if i=='a'or i=='e' or i=='i' or i=='o' or i=='u':
+            c+=1
+    a.append(c)
+print(min(a))
