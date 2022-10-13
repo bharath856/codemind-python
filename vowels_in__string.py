f6d1203ae8db6f92e81b5524e7c78a836c4fba79
bharath856 <99651027+bharath856@users.noreply.github.com>
2022-10-13 20:26:06 +0530

    s=input() a=[] for i in s:     if (i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U')and i not in a:         a.append(i)         print(i,end=' ')

---
+++ vowels_in__string.py
@@ -0,0 +1,6 @@
+s=input()
+a=[]
+for i in s:
+    if (i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U')and i not in a:
+        a.append(i)
+        print(i,end=' ')
