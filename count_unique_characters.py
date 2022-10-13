fa8a142b9ccea7adbb5f6455232cc890568923a0
bharath856 <99651027+bharath856@users.noreply.github.com>
2022-10-13 22:30:17 +0530

    s=input() r=s.lower() c=0 for i in r:     if r.count(i)==1:         if i!=' ':             c+=1 print(c)

--- count_unique_characters.py
+++ count_unique_characters.py
@@ -1,13 +1,8 @@
-a=input()
-c=co=0
-for i in a:
-    c=0
-    if i==" ":
-        continue
-    else:
-        for j in a:
-            if i==j or i==j.upper() or i==j.lower():
-                c+=1
-        if c==1:
-            co+=1
-print(co)
+s=input()
+r=s.lower()
+c=0
+for i in r:
+    if r.count(i)==1:
+        if i!=' ':
+            c+=1
+print(c)
